5f007055e05ca16c6095cad2a356b795d3844f54
liushepeng <liushepeng@yottachain.io>
2019-04-29 16:20:20 +0800

    YTSN

--- src/main/java/com/ytfs/service/http/HttpServerBoot.java
+++ src/main/java/com/ytfs/service/http/HttpServerBoot.java
@@ -1,10 +1,25 @@
 package com.ytfs.service.http;
 
+import java.io.IOException;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
+
 public class HttpServerBoot {
-    
-    
-    public static void startHttpServer(){
-        
+
+    public static void startHttpServer() throws IOException {
+        HttpServer httpServer = new HttpServer();
+        NetworkListener networkListener = new NetworkListener("sample-listener", "127.0.0.1", 18888);
+        ThreadPoolConfig threadPoolConfig = ThreadPoolConfig
+                .defaultConfig()
+                .setCorePoolSize(1)
+                .setMaxPoolSize(1);
+        networkListener.getTransport().setWorkerThreadPoolConfig(threadPoolConfig);
+        httpServer.addListener(networkListener);
+        HttpHandler httpHandler = new UseSpaceHandler();
+        httpServer.getServerConfiguration().addHttpHandler(httpHandler, new String[]{"/sample"});
+        httpServer.start();
     }
 
 }
---
+++ src/main/java/com/ytfs/service/http/UseSpaceHandler.java
@@ -0,0 +1,15 @@
+package com.ytfs.service.http;
+
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+
+
+public class UseSpaceHandler extends HttpHandler {
+
+    @Override
+    public void service(Request rqst, Response rspns) throws Exception {
+        
+    }
+    
+}
