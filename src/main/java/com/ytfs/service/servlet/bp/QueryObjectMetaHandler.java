3ac394be9fe0a38dd8ff40eadb1268e1a695b351
liushepeng <liushepeng@sohu.com>
2019-05-23 12:59:28 +0800

    SN

--- src/main/java/com/ytfs/service/servlet/bp/QueryObjectMetaHandler.java
+++ src/main/java/com/ytfs/service/servlet/bp/QueryObjectMetaHandler.java
@@ -1,18 +1,18 @@
 package com.ytfs.service.servlet.bp;
 
 import com.ytfs.common.conf.ServerConfig;
-import com.ytfs.service.dao.ObjectAccessor;
 import com.ytfs.service.dao.ObjectMeta;
 import com.ytfs.common.net.P2PUtils;
 import com.ytfs.common.node.SuperNodeList;
 import com.ytfs.service.servlet.Handler;
-import static com.ytfs.common.ServiceErrorCode.INVALID_UPLOAD_ID;
 import com.ytfs.common.ServiceException;
+import com.ytfs.service.packet.ObjectRefer;
 import com.ytfs.service.packet.QueryObjectMetaReq;
 import com.ytfs.service.packet.QueryObjectMetaResp;
+import com.ytfs.service.servlet.ReferCache;
 import io.yottachain.nodemgmt.core.vo.SuperNode;
+import java.util.List;
 import org.apache.log4j.Logger;
-import org.bson.types.ObjectId;
 
 public class QueryObjectMetaHandler extends Handler<QueryObjectMetaReq> {
 
@@ -36,21 +36,10 @@ public class QueryObjectMetaHandler extends Handler<QueryObjectMetaReq> {
 
     private static QueryObjectMetaResp queryObjectMeta(QueryObjectMetaReq req) throws ServiceException {
         LOG.info("Query object meta:" + req.getUserID() + "/" + req.getVNU());
-        ObjectMeta meta = queryObjectMeta(req.getVNU(), req.getUserID());
-        QueryObjectMetaResp resp = new QueryObjectMetaResp(meta.getBlocks(), meta.getLength());
+        List<ObjectRefer> refers = ReferCache.getRefersCache(req.getVNU(), req.getUserID());
+        long len = ReferCache.getObjectLength(req.getVNU(), req.getUserID());
+        QueryObjectMetaResp resp = new QueryObjectMetaResp(refers, len);
         return resp;
     }
 
-    static ObjectMeta queryObjectMeta(ObjectId VNU, int UserID) throws ServiceException {
-        ObjectMeta meta = ObjectAccessor.getObject(VNU);
-        if (meta == null) {
-            throw new ServiceException(INVALID_UPLOAD_ID);
-        } else {
-            if (meta.getUserID() != UserID) {
-                throw new ServiceException(INVALID_UPLOAD_ID);
-            } else {
-                return meta;
-            }
-        }
-    }
 }
--- src/main/java/com/ytfs/service/servlet/bp/SaveObjectMetaHandler.java
+++ src/main/java/com/ytfs/service/servlet/bp/SaveObjectMetaHandler.java
@@ -1,16 +1,16 @@
 package com.ytfs.service.servlet.bp;
 
+import static com.ytfs.common.ServiceErrorCode.INVALID_UPLOAD_ID;
 import com.ytfs.common.conf.ServerConfig;
 import com.ytfs.service.dao.ObjectAccessor;
-import com.ytfs.service.dao.ObjectMeta;
 import com.ytfs.common.net.P2PUtils;
 import com.ytfs.common.node.SuperNodeList;
 import com.ytfs.service.servlet.Handler;
-import static com.ytfs.service.servlet.bp.QueryObjectMetaHandler.queryObjectMeta;
 import com.ytfs.common.ServiceException;
 import com.ytfs.service.packet.ObjectRefer;
 import com.ytfs.service.packet.SaveObjectMetaReq;
 import com.ytfs.service.packet.SaveObjectMetaResp;
+import com.ytfs.service.servlet.ReferCache;
 import io.yottachain.nodemgmt.core.vo.SuperNode;
 import java.util.List;
 import org.apache.log4j.Logger;
@@ -41,22 +41,27 @@ public class SaveObjectMetaHandler extends Handler<SaveObjectMetaReq> {
     }
 
     private static SaveObjectMetaResp saveObjectMeta(SaveObjectMetaReq request) throws ServiceException {
-        LOG.info("Save object meta:" + request.getUserID() + "/" + request.getVNU());
-        SaveObjectMetaResp resp = new SaveObjectMetaResp();
-        ObjectMeta meta = queryObjectMeta(request.getVNU(), request.getUserID());
-        List<ObjectRefer> refers = ObjectRefer.parse(meta.getBlocks());
-        resp.setExists(false);
-        for (ObjectRefer refer : refers) {
-            if (refer.getId() == request.getRefer().getId()) {
-                resp.setExists(true);
-                break;
-            }
+        LOG.info("Save object meta:" + request.getUserID() + "/" + request.getVNU()
+                + "/" + request.getRefer().getId() + "/" + request.getRefer().getVBI());
+        if (request.getRefer().getVBI() == 0) {
+            throw new ServiceException(INVALID_UPLOAD_ID);
         }
-        if (!resp.isExists()) {
-            long usedspace = sumUsedSpace(request.getRefer().getRealSize(), request.getNlink());
-            refers.add(request.getRefer());
-            byte[] bs = ObjectRefer.merge(refers);
-            ObjectAccessor.updateObject(request.getVNU(), bs, usedspace);
+        SaveObjectMetaResp resp = new SaveObjectMetaResp();
+        List<ObjectRefer> refers = ReferCache.getRefersCache(request.getVNU(), request.getUserID());
+        resp.setExists(false);
+        synchronized (refers) {
+            for (ObjectRefer refer : refers) {
+                if (refer.getId() == request.getRefer().getId()) {
+                    resp.setExists(true);
+                    break;
+                }
+            }
+            if (!resp.isExists()) {
+                long usedspace = sumUsedSpace(request.getRefer().getRealSize(), request.getNlink());
+                refers.add(request.getRefer());
+                byte[] bs = ObjectRefer.merge(refers);
+                ObjectAccessor.updateObject(request.getVNU(), bs, usedspace);
+            }
         }
         return resp;
     }
