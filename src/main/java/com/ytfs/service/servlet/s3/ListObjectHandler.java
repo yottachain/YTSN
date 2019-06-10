bb3199456ef335ad5f46b9a4d7ce32e35790fd73
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-06-10 10:59:34 +0800

    Code refactoring

--- src/main/java/com/ytfs/service/servlet/s3/ListObjectHandler.java
+++ src/main/java/com/ytfs/service/servlet/s3/ListObjectHandler.java
@@ -1,17 +1,13 @@
 package com.ytfs.service.servlet.s3;
 
-import com.ytfs.service.dao.BucketCache;
-import com.ytfs.service.dao.BucketMeta;
-import com.ytfs.service.dao.FileAccessor;
-import com.ytfs.service.dao.User;
+import com.ytfs.service.dao.*;
 import com.ytfs.service.packet.s3.ListObjectReq;
 import com.ytfs.service.packet.s3.ListObjectResp;
 import com.ytfs.service.servlet.Handler;
 import org.apache.log4j.Logger;
-import org.bson.types.ObjectId;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.List;
 
 public class ListObjectHandler extends Handler<ListObjectReq> {
 
@@ -22,13 +18,13 @@ public class ListObjectHandler extends Handler<ListObjectReq> {
         User user = this.getUser();
         LOG.info("LIST object:" + user.getUserID());
         int limit = request.getLimit();
+        String prefix = request.getPrefix();
         BucketMeta meta = BucketCache.getBucket(user.getUserID(), request.getBucketName(),null);
         String fileName = request.getFileName();
-        Map<String,byte[]> map = new LinkedHashMap<>();
-        String lastFileName = FileAccessor.listObjectByBucket(map, meta.getBucketId(), fileName, limit);
+
+        List<FileMetaV2> fileMetaV2s = FileAccessorV2.listBucket(meta.getBucketId(), fileName, null,  prefix,  limit);
         ListObjectResp resp = new ListObjectResp();
-        resp.setMap(map);
-        resp.setFileName(lastFileName);
+        resp.setObjects(Collections.singletonList(fileMetaV2s));
         return resp;
     }
 
