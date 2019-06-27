10bfe4eaa9fa93346e073f57c214f027e3917d31
liushepeng <liushepeng@sohu.com>
2019-06-27 09:48:40 +0800

    SN

---
+++ src/main/java/com/ytfs/service/AddSuperNode.java
@@ -0,0 +1,11 @@
+package com.ytfs.service;
+
+public class AddSuperNode {
+
+    public static void main(String[] args) {
+        if (args != null && args.length == 1) {
+            
+        }
+    }
+
+}
